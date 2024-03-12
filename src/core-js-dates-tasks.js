b84d52c0dcf2f7b103eee9b786b379e5a4e27625
Aleksei Golubkov <nuriver@gmail.com>
2024-03-12 22:23:46 +0100

    feat: finish getCountDaysOnPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -125,8 +125,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const msBetween = new Date(dateEnd).getTime() - new Date(dateStart).getTime();
+  const daysBetween = Math.round(msBetween / (1000 * 3600 * 24)) + 1;
+  return daysBetween;
 }
 
 /**
